13d6abaebfd982b996a2143ebaa4f5e3ffb8e0cf
urataps <alexeluca.spataru@gmail.com>
2024-02-10 14:59:58 +0200

    test: setup"

---
+++ test/Base.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.23 <0.9.0;
+
+import { PRBTest } from "@prb/test/src/PRBTest.sol";
+import { StdCheats } from "forge-std/src/StdCheats.sol";
+
+contract BaseTest is PRBTest, StdCheats {
+    struct Users {
+        address admin;
+        address stranger;
+        address vault;
+    }
+
+    Users public users;
+
+    function setUp() public virtual {
+        users = Users({
+            admin: createUser("admin");,
+            stranger: createUser("stranger"),
+            vault: createUser("vault")
+        });
+
+        vm.startPrank(users.admin);
+    }
+
+    function createUser(string memory name) public returns(address account) {
+        account = makeAddr(name);
+        deal(account, type(uint256).max);
+    }
+}
---
+++ test/erc404/ERC404.t.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.23;
+
+import { BaseTest } from "test/Base.t.sol";
+
+contract ERC404Test is BaseTest { }
