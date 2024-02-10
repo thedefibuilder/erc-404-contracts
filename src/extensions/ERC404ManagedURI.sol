d300ec88557d3ba38160037000b29c1cb17bb021
urataps <alexeluca.spataru@gmail.com>
2024-02-10 14:14:29 +0200

    fix: base uri

--- src/extensions/ERC404ManagedURI.sol
+++ src/extensions/ERC404ManagedURI.sol
@@ -2,16 +2,17 @@
 pragma solidity >=0.8.23;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { ERC404, IERC404 } from "src/ERC404.sol";
 
 contract ERC404ManagedURI is Ownable, ERC404 {
+    using Strings for uint256;
+
     event MetadataUpdate(uint256 tokenId);
     event BatchMetadataUpdate(uint256 fromTokenId, uint256 toTokenId);
 
     bytes4 private constant ERC4906_INTERFACE_ID = bytes4(0x49064906);
-
-    uint256 public totalRegisteredURIs;
-    mapping(uint256 uriId => string uri) private _tokenURIs;
+    string public baseURI;
 
     constructor(
         string memory name,
@@ -27,24 +28,29 @@ contract ERC404ManagedURI is Ownable, ERC404 {
         return super.owner();
     }
 
-    // TODO: Add batch tokenURI register support
-
-    function registerTokenURI(string memory uri) public onlyOwner {
-        _tokenURIs[totalRegisteredURIs] = uri;
-        totalRegisteredURIs++;
-
-        if (minted > totalRegisteredURIs) {
+    function setBaseURI(string memory uri) public onlyOwner {
+        if (bytes(baseURI).length > 0) {
             emit BatchMetadataUpdate(0, minted);
         }
+
+        baseURI = uri;
     }
 
     /// @inheritdoc IERC404
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         _requireOwned(tokenId);
 
-        // TODO: Improve the URI selection
-        uint256 uriId = uint256(keccak256(abi.encode(tokenId))) % totalRegisteredURIs;
-        return _tokenURIs[uriId];
+        string memory base = baseURI;
+
+        if (bytes(base).length > 0) {
+            // This is necessary because tokenId can exceed the totalNFTSupply
+            uint256 seed = uint256(keccak256(abi.encodePacked(tokenId)));
+            uint256 uriId = seed % (totalSupply / _UNIT);
+
+            return string.concat(base, uriId.toString());
+        }
+
+        return "";
     }
 
     function supportsInterface(bytes4 interfaceId) public view override returns (bool) {
